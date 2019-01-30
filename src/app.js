1baac1a0b7e3aa190871a69ea96ef6e22da2ee69
Nikita <origami1024@gmail.com>
2019-01-30 22:30:41 +0300

    some some

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/*
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+*/
+port = 8000;
+var express = require('express')
+console.log("test1")
+
+//document.writeln("jojo")
