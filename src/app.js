8488cba3f1edea01db9f6a579ed80a5910fda296
Nikita <origami1024@gmail.com>
2019-01-30 22:35:13 +0300

    some some yet

--- src/app.js
+++ src/app.js
@@ -8,6 +8,23 @@ app.listen(port);
 */
 port = 8000;
 var express = require('express')
-console.log("test1")
+var app = express();
+
+var http = require('http')
+var server = http.server(app)
+
+app.use(express.static('client'))
+
+server.listen(PORT, function() {
+    console.log('server running')
+})
+
+var io = require('socket.io')(server)
+
+io.on('connection', function(socket) {
+    socket.on('message', function(msg){
+        io.emit('message', msg)
+    })
+})
 
 //document.writeln("jojo")
